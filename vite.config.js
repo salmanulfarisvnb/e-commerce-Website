474b8e1be6693577b9d42fd301761cfc644505f7
salmanulfarisvnb <salmanulfarisp9946@gmail.com>
2024-10-26 10:02:34 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/salmanulfarisvnb/",
+  base: "/e-commerce-Website/",
   plugins: [react()],
 });
