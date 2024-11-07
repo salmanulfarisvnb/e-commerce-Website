3053d32083e47f265cc24184c09b18e578913e86
salmanulfarisvnb <salmanulfarisp9946@gmail.com>
2024-11-07 12:38:54 +0530

    order added

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Title } from "../components";
+import { shopContext } from "../context/ShopContext";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { products, currency } = useContext(shopContext);
+  return (
+    <div className="mt-5 border-t">
+      <div className="mt-10 text-lg sm:text-xl">
+        <Title text1={"MY"} text2={"ORDER"} />
+      </div>
+      <div>
+        {products.map((item, i) => {
+          <h1 key={i}>{item.name}</h1>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
