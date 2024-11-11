d6bceddfbbded9e3df451fed924e08cebfda750f
salmanulfarisvnb <salmanulfarisp9946@gmail.com>
2024-11-11 16:20:41 +0530

    about page complated

--- src/pages/Abuot.jsx
+++ src/pages/Abuot.jsx
@@ -26,7 +26,7 @@ const Abuot = () => {
             selection of high-quality products that cater to every taste and
             preference. From fashion and beauty to electronics and home
             essentials, we offer an extensive collection sourced from trusted
-            brands and suppliers.
+            brands and suppliers
           </p>
           <h1 className="my-5 text-base font-semibold">Our Mission</h1>
           <p>
@@ -34,6 +34,32 @@ const Abuot = () => {
             convenience, and confidence. We're dedicated to providing a seamless
             shopping experience that exceeds expectations, from browsing and
             ordering to delivery and beyond.
+          </p>
+        </div>
+      </div>
+      <div className="text-2xl mt-7">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="grid grid-cols-1 mt-4 sm:grid-cols-2 md:grid-cols-3">
+        <div className="p-16 border">
+          <h1 className="font-semibold ">Quality Assurance:</h1>
+          <p className="mt-3 font-light">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+        <div className="p-16 border">
+          <h1 className="font-semibold ">Quality Assurance:</h1>
+          <p className="mt-3 font-light">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+        <div className="items-center justify-center p-16 border">
+          <h1 className="font-semibold ">Quality Assurance:</h1>
+          <p className="mt-3 font-light">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
           </p>
         </div>
       </div>
